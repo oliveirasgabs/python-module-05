182bd1a26e528fef9863e6297bd561a079ffbb8b
Gabrielly <oliveiragabrielly.dev@gmail.com>
2026-04-14 06:39:21 -0300

    inserindo funcionamento do validate e ingest

--- ex0/data_processor.py
+++ ex0/data_processor.py
@@ -4,7 +4,8 @@ from typing import Any
 
 class DataProcessor(ABC):
     def __init__(self):
-        buffer: list[Any] = []
+        self._buffer: list[Any] = []
+
 
     @abstractmethod
     def validate(self, data: Any) -> bool:
@@ -18,32 +19,62 @@ class DataProcessor(ABC):
         return [numeric: int, string: str]
     pass
 
-#abobrinha
+
 class NumericProcessor(DataProcessor):
     def validate(self, data: Any) -> bool:
-        if isinstance(data, int | float | list[int | float]):
-            
+        if isinstance(data, int | float):
+            return True
+        elif isinstance(data, list) and all(isinstance(i, int | float) for i in data):
             return True
         else:
+            return False
+
+    def ingest(self, data: int | float | list[int | float]) -> None:
+        if not self.validate(data):
             raise TypeError("Improper numeric data")
-
-    def ingest(self, data: int | float | list[int | float]):
-
-        return super().ingest()
+        if isinstance(data, list):
+            for item in data:
+                self._buffer.append(str(item))
+        else:
+            self._buffer.append(str(data))
 
 
 class TextProcessor(DataProcessor):
     def validate(self, data: Any) -> bool:
         if isinstance(data, str):
-        return
+            return True
+        elif isinstance(data, list) and all(isinstance(i, str) for i in data:
+            return True
+        else:
+            return False
 
-    def ingest(self, data: str | list[str]):
-        return
+    def ingest(self, data: str | list[str]) -> None:
+        if not self.validate(data):
+            raise TypeError("Improper text data")
+        if isinstance(data, list):
+            for item in data:
+                self._buffer.append(item)
+        else:
+            self._buffer.append(data)
 
 
 class LogProcessor(DataProcessor):
     def validate(self, data: Any) -> bool:
-        return
+        if isinstance(data, dict) and all(isinstance(k, str) and isinstance(v, str) for k, v in data.items()):
+            return True
+        elif isinstance(data, list) and all(isinstance(i, dict) and all(isinstance(k, str) and isinstance(v, str) for k, v in i.items()) for i in data):
+            return True
+        else:
+            return False
 
-    def ingest(self, data: dict{}):
-        return
+
+    def ingest(self, data: dict[str, str] | list[dict[str, str]]) -> None:
+        if not self.validate(data):
+            raise TypeError("Improper log data")
+        if isinstance(data, list):
+            for key in data:
+                log_str: str = ": ".join(key.values())
+                self._buffer.append(log_str)
+        else:
+            log_str: str = ": ".join(data.values())
+            self._buffer.append(log_str)
